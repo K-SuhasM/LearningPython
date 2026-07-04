6c58ecba2f15314fee9272c59c307ab50c350886
K-SuhasM <k.suhasmandle@gmail.com>
2026-07-04 20:37:17 +0530

    Sets, Dictionaries, For loop with else

---
+++ Day7.py
@@ -0,0 +1,45 @@
+# so= {1,2,3,4,5,6,7,8,9}
+# st= {2,4,5,34,6,3,45,234,6}
+
+# # print(so.union(st))
+# so.add(9)
+# so.add(9)
+# so.add(9)
+# so.add(9)
+# so.add(9)
+# print(so)
+
+# dict= {"Me" : 7041,
+#        "baba" : 9893}
+
+
+# for i in dict:
+#     print(i, dict[i])
+    
+
+# em  = {1:45, 2:80, 3:86, 4:43, 5:54, 6:43, 7:96, 8:45, 9:35}
+# em2 = {10:35, 11:45, 12:24, 13:43, 14:42, 15:65, 16:23, 17:43, 18:86}
+
+
+# print(em)
+# # em.update(em2) 
+# # em.clear()
+# # em2.pop(17)
+# # em.popitem()
+# # del em[3]
+# print(em)
+
+
+for i in range(5):
+    print(i)
+# else:
+print("done")
+
+
+
+
+
+
+# for i in em:
+#     print(em[i])
+# # print(c)
