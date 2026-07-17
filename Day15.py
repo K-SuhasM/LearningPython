b8954a25350507727c1671a02990b1c6b3c8145b
K-SuhasM <k.suhasmandle@gmail.com>
2026-07-17 23:00:34 +0530

    Classes

---
+++ Day15.py
@@ -0,0 +1,25 @@
+# class employee:
+#     comp="sbi"
+    
+#     def __init__(self, name):
+#         self.name=name
+#         self.roll=25
+#     def showdetails(self):
+#         print(f"The name of employee is {self.name} and his roll is {self.roll}.....{self.comp}")
+#     def changecom (self, com):
+#         self.comp=com
+# em1 = employee("Koustubh")
+# em2 = employee("Rohit")
+# em3 = employee("sam")
+
+# em1.showdetails()
+# em1.changecom("hdfc")
+# em1.showdetails()
+
+
+
+inp= input("ENter: ")
+
+a=inp.split(" ")
+for i in a:
+    print(i)
--- text.py
+++ text.py
@@ -1,8 +1,17 @@
-while True:
-    num=input("Enter your numbers: ")
-    numb=[]
-    for i in num:
-        i=int(i)
-        numb.append(i)
-    print(sum(numb))  
-    a=2s
+# while True:
+#     num=input("Enter your numbers: ")
+#     numb=[]
+#     for i in num:
+#         i=int(i)
+#         numb.append(i)
+#     print(sum(numb))  
+#     a=2
+
+
+
+
+# Porsche 
+# BMW
+
+# x = ["a", "b"]
+# print(list(enumerate(x)))
