5911fdee4d39285d8987b91f8e8f919bef22ffe8
K-SuhasM <k.suhasmandle@gmail.com>
2026-07-22 10:32:14 +0530

    Classes and Time module

---
+++ Day18.py
@@ -0,0 +1,25 @@
+# import time
+# def forr():
+#     for i in range(50000):
+#         print(i)
+
+# def whilee():
+#     i=0
+#     while i<500:
+#         i=i+1
+#         print(i)
+
+# t=time.time()
+# forr()
+# c=(time.time()-t)
+
+# t2=time.time()
+# whilee()
+# c2=(time.time()-t2)
+
+# print(c)
+# print(c2)
+import time
+
+
+print(time.localtime())
